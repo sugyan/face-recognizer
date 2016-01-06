50a55b6b608e74f247163d1de8716a62b23c5996
sugyan <sugi1982@gmail.com>
2016-01-06 22:58:06 +0900

    add root page

---
+++ gulpfile.js
@@ -0,0 +1,15 @@
+/* eslint-env node */
+var gulp = require('gulp');
+var babel = require('gulp-babel');
+
+gulp.task('build', function() {
+    return gulp.src('app/assets/src/js/*.js')
+        .pipe(babel({ presets: ['es2015'] }))
+        .pipe(gulp.dest('app/assets/javascripts/dest'));
+});
+
+gulp.task('watch', function() {
+    gulp.watch('app/assets/src/js/*.js', ['build']);
+});
+
+gulp.task('default', ['build']);
