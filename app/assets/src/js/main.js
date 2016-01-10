dc7babc1b147f9c324ac9987a1916e120dd6ef40
sugyan <sugi1982@gmail.com>
2016-01-10 23:47:43 +0900

    update js

--- app/assets/src/js/main.js
+++ app/assets/src/js/main.js
@@ -1,8 +1,9 @@
 /* global $, EXIF */
 class Main {
-    constructor(canvas, loading) {
+    constructor(canvas, loading, response) {
         this.canvas = canvas;
         this.loading = loading;
+        this.response = response;
         this.size = Math.min($(window).width() - 30, 512);
         this.canvas.width = this.canvas.height = this.size;
         $(this.loading)
@@ -15,55 +16,6 @@ class Main {
         this.ctx = canvas.getContext('2d');
         this.ctx.fillStyle = '#000';
 
-        // image load
-        this.image = new window.Image();
-        this.image.onload = () => {
-            // draw to canvas
-            const h = this.image.height;
-            const w = this.image.width;
-            this.scale = Math.max(w / this.size, h / this.size);
-            this.offset_x = (this.size - w / this.scale) / 2.0;
-            this.offset_y = (this.size - h / this.scale) / 2.0;
-            this.ctx.fillRect(0, 0, this.size, this.size);
-            const transform = {
-                1: [1, 0, 0, 1, 0, 0],
-                2: [-1, 0, 0, 1, this.size, 0],
-                3: [-1, 0, 0, -1, this.size, this.size],
-                4: [1, 0, 0, -1, 0, this.size],
-                5: [0, 1, 1, 0, 0, 0],
-                6: [0, 1, -1, 0, this.size, 0],
-                7: [0, -1, -1, 0, this.size, this.size],
-                8: [0, -1, 1, 0, 0, this.size]
-            };
-            this.ctx.transform(...transform[this.orientation || 1]);
-            this.ctx.drawImage(this.image, this.offset_x, this.offset_y, w / this.scale, h / this.scale);
-            this.ctx.setTransform(...transform[1]);
-            if ((this.orientation || 1) > 4) {
-                [this.offset_x, this.offset_y] = [this.offset_y, this.offset_x];
-            }
-            // post to api
-            const key = this.key = Math.floor(Math.random() * 0xFFFFFFFF).toString(16);
-            $(this.loading).show();
-            $.ajax({
-                url: '/api',
-                method: 'POST',
-                data: {
-                    image: this.image.src
-                },
-                success: (result) => {
-                    if (this.key !== key) {
-                        return;
-                    }
-                    $(this.loading).hide();
-                    $('#response').text(JSON.stringify(result, null, '  '));
-                    this.drawFaceRect(result.faces);
-                }
-            });
-        };
-        this.image.onerror = () => {
-            this.ctx.fillRect(0, 0, this.size, this.size);
-            alert('Failed to load image.');
-        };
         // drag and drop
         this.canvas.addEventListener('dragover', (e) => {
             e.preventDefault();
@@ -73,6 +25,54 @@ class Main {
             e.preventDefault();
             if (e.dataTransfer.files.length > 0) {
                 this.readFile(e.dataTransfer.files[0]);
+            }
+        });
+    }
+    onImageLoad() {
+        // draw to canvas
+        const h = this.image.height;
+        const w = this.image.width;
+        this.scale = Math.max(w / this.size, h / this.size);
+        this.offset_x = (this.size - w / this.scale) / 2.0;
+        this.offset_y = (this.size - h / this.scale) / 2.0;
+        this.ctx.fillRect(0, 0, this.size, this.size);
+        // rotate image
+        EXIF.getData(this.image, () => {
+            const transforms = {
+                1: [1, 0, 0, 1, 0, 0],
+                2: [-1, 0, 0, 1, this.size, 0],
+                3: [-1, 0, 0, -1, this.size, this.size],
+                4: [1, 0, 0, -1, 0, this.size],
+                5: [0, 1, 1, 0, 0, 0],
+                6: [0, 1, -1, 0, this.size, 0],
+                7: [0, -1, -1, 0, this.size, this.size],
+                8: [0, -1, 1, 0, 0, this.size]
+            };
+            const orientation = EXIF.getTag(this.image, 'Orientation');
+            this.ctx.transform(...transforms[orientation || 1]);
+            this.ctx.drawImage(this.image, this.offset_x, this.offset_y, w / this.scale, h / this.scale);
+            this.ctx.setTransform(...transforms[1]);
+            if ((orientation || 1) > 4) {
+                [this.offset_x, this.offset_y] = [this.offset_y, this.offset_x];
+            }
+        });
+        // post to api
+        const key = this.key = Math.floor(Math.random() * 0xFFFFFFFF).toString(16);
+        $(this.response).text('');
+        $(this.loading).show();
+        $.ajax({
+            url: '/api',
+            method: 'POST',
+            data: {
+                image: this.image.src
+            },
+            success: (result) => {
+                if (this.key !== key) {
+                    return;
+                }
+                $(this.loading).hide();
+                $(this.response).text(JSON.stringify(result, null, '  '));
+                this.drawFaceRect(result.faces);
             }
         });
     }
@@ -86,15 +86,14 @@ class Main {
     readFile(file) {
         const reader = new window.FileReader();
         reader.onload = (e) => {
-            // read exif data (use new Image object)
-            const img = new window.Image();
-            img.onload = () => {
-                EXIF.getData(img, () => {
-                    this.orientation = EXIF.getTag(img, 'Orientation');
-                    this.image.src = e.target.result;
-                });
+            // load image
+            this.image = new window.Image();
+            this.image.onload = this.onImageLoad.bind(this);
+            this.image.onerror = () => {
+                this.ctx.fillRect(0, 0, this.size, this.size);
+                alert('Failed to load image.');
             };
-            img.src = e.target.result;
+            this.image.src = e.target.result;
         };
         reader.readAsDataURL(file);
     }
@@ -132,7 +131,8 @@ class Main {
 document.addEventListener('DOMContentLoaded', () => {
     const main = new Main(
         document.getElementById('canvas'),
-        document.getElementById('loading')
+        document.getElementById('loading'),
+        document.getElementById('response')
     );
     main.enableSelectFile(document.getElementById('file'));
 });
